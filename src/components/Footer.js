a32aa0db907890a4922eb5f657874318d64b110f
Ajay Godara <agodara771@gmail.com>
2025-07-09 10:48:19 +0530

    Navbar & Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white border-t mt-10 px-6 py-6 text-sm text-center text-gray-500">
+      <div className="flex justify-center gap-4 mb-2">
+        <a href="#"><i className="fab fa-twitter" /></a>
+        <a href="#"><i className="fab fa-instagram" /></a>
+        <a href="#"><i className="fab fa-facebook" /></a>
+      </div>
+      <p>©2024 Whatbytes. All rights reserved.</p>
+    </footer>
+  );
+}
